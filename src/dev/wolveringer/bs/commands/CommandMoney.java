3dd425720a6f578785ef396c3249a9a7d704a30e
kingingo <kingingohd@googlemail.com>
2016-10-02 23:35:22 +0200

    change CMD -> money to gmoney

--- src/dev/wolveringer/bs/commands/CommandMoney.java
+++ src/dev/wolveringer/bs/commands/CommandMoney.java
@@ -8,7 +8,7 @@ import net.md_5.bungee.api.plugin.Command;
 
 public class CommandMoney extends Command{ //TODO improve
 	public CommandMoney() {
-		super("money");
+		super("gmoney");
 	}
 	
 	public void execute(CommandSender cs, String[] args) {
