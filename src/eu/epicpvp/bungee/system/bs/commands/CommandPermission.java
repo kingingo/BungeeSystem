1b2c2974484fde1e712c0d322aba355b30e4d305
Janmm14 <computerjanimaus@yahoo.de>
2016-10-10 00:25:37 +0200

    Bessere Chatausgaben für /perm

--- src/eu/epicpvp/bungee/system/bs/commands/CommandPermission.java
+++ src/eu/epicpvp/bungee/system/bs/commands/CommandPermission.java
@@ -2,216 +2,210 @@ package eu.epicpvp.bungee.system.bs.commands;
 
 import java.util.UUID;
 
-import org.apache.commons.lang3.StringUtils;
-
 import eu.epicpvp.bungee.system.bs.Main;
 import eu.epicpvp.bungee.system.permission.Group;
 import eu.epicpvp.bungee.system.permission.Permission;
 import eu.epicpvp.bungee.system.permission.PermissionManager;
 import eu.epicpvp.bungee.system.permission.PermissionPlayer;
+import eu.epicpvp.datenclient.client.LoadedPlayer;
 import eu.epicpvp.datenserver.definitions.permissions.PermissionType;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Listener;
+import org.apache.commons.lang3.StringUtils;
 
 public class CommandPermission extends Command implements Listener {
-
+	
 	public CommandPermission(String name) {
 		super(name);
 	}
-
+	
 	@Override
 	public void execute(CommandSender cs, String[] args) {
-		if(cs instanceof ProxiedPlayer){
+		if (cs instanceof ProxiedPlayer) {
 			ProxiedPlayer p = (ProxiedPlayer) cs;
-			if(!PermissionManager.getManager().hasPermission(p, PermissionType.PERMISSION,true))
+			if (!PermissionManager.getManager().hasPermission(p, PermissionType.PERMISSION, true))
 				return;
 		}
-		if(args.length == 2){
-			if(args[0].equalsIgnoreCase("userInfo")){
+		if (args.length == 2) {
+			if (args[0].equalsIgnoreCase("userInfo")) {
 				UUID uuid = null;
-				String name = null;
-				if(args[1].matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")){
+				LoadedPlayer loadedPlayer = null;
+				if (args[1].matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")) {
 					uuid = UUID.fromString(args[1]);
-					name = Main.getDatenServer().getClient().getPlayerAndLoad(uuid).getName();
-				}
-				else if(args[1].length()<=16){
-					uuid = Main.getDatenServer().getClient().getPlayerAndLoad(args[1]).getUUID();
-				}
-				else{
-					cs.sendMessage("§cPlayer \""+args[1]+"\" not found");
+					loadedPlayer = Main.getDatenServer().getClient().getPlayerAndLoad(uuid);
+				} else if (args[1].length() <= 16) {
+					loadedPlayer = Main.getDatenServer().getClient().getPlayerAndLoad(args[1]);
+					uuid = loadedPlayer.getUUID();
+				} else {
+					cs.sendMessage("§cSpielername \"" + args[1] + "\" nicht valid");
 					return;
 				}
 				PermissionPlayer pp = PermissionManager.getManager().getPlayer(uuid);
-				cs.sendMessage("§aPermissions for §b"+name);
-				cs.sendMessage("§aUUID: §b"+uuid);
-				cs.sendMessage("§aOwned permissions:");
-				for(Permission x : pp.getPermissions())
-					cs.sendMessage("  §7- §b"+x.getPermission()+"§7-§b"+x.getGroup());
-				cs.sendMessage("§aOwned negative permissions:");
-				for(Permission x : pp.getNegativePermissions())
-					cs.sendMessage("  §7- §b"+x.getPermission()+"§7-§b"+x.getGroup());
-				cs.sendMessage("§aGroup permissions:");
-				for(Group g : pp.getGroups())
-					if(g != null){
-						cs.sendMessage("  §6Group §a"+g.getName()+"§7[§r"+g.getPrefix()+"§7] Importance: "+g.getImportance());
-						for(Permission x : g.getPermissions())
-							cs.sendMessage("  §7- §b"+x.getPermission()+"§7-§b"+x.getGroup());
-						for(Permission x : g.getNegativePerms())
-							cs.sendMessage("  §7- §b"+x.getPermission()+"§7-§b"+x.getGroup());
+				cs.sendMessage("§aRechte von §b" + loadedPlayer.getName());
+				cs.sendMessage("§aUUID: §b" + uuid);
+				cs.sendMessage("§aPersönliche Berechtigungen:");
+				for (Permission p : pp.getPermissions())
+					cs.sendMessage("  §7- §b" + p.getPermission() + "§7-§b" + p.getGroup());
+				cs.sendMessage("§aPersönliche negative Berechtigungen:");
+				for (Permission p : pp.getNegativePermissions())
+					cs.sendMessage("  §7- §b" + p.getPermission() + "§7-§b" + p.getGroup());
+				cs.sendMessage("§aGruppenrechte:");
+				for (Group g : pp.getGroups())
+					if (g != null) {
+						cs.sendMessage("  §6Gruppe §a" + g.getName() + "§7[§r" + g.getPrefix() + "§7] Importance: " + g.getImportance());
+						for (Permission x : g.getPermissions())
+							cs.sendMessage("  §7- §b" + x.getPermission() + "§7-§b" + x.getGroup());
+						for (Permission x : g.getNegativePerms())
+							cs.sendMessage("  §7- §b" + x.getPermission() + "§7-§b" + x.getGroup());
 					}
 				return;
-			}
-			else if(args[0].equalsIgnoreCase("groupInfo")){
+			} else if (args[0].equalsIgnoreCase("groupInfo")) {
 				Group g = PermissionManager.getManager().getGroup(args[1]);
-				if(g != null){
-					cs.sendMessage("§aGroup: §e"+g.getName()+"§7[§r"+g.getPrefix()+"§7]");
-					cs.sendMessage("§aGroup instances:");
-					for(Group id : g.getInstances()){
-						cs.sendMessage("  §7- "+id.getName());
-					}
-					cs.sendMessage("§aPermissions:");
-					for(Permission p : g.getPermissions()){
-						cs.sendMessage("  §7- "+p.getPermission());
-					}
-					cs.sendMessage("§aNegative Permissions:");
-					for(Permission p : g.getNegativePerms()){
-						cs.sendMessage("  §7- "+p.getPermission());
-					}
+				if (g == null) {
+					cs.sendMessage("§cGruppe nicht gefunden.");
+					return;
 				}
-				else
-				{
-					cs.sendMessage("§cGroup not found.");
+				cs.sendMessage("§aGruppe: §e" + g.getName() + "§7[§r" + g.getPrefix() + "§7]");
+				cs.sendMessage("§aVererbende Gruppen:");
+				for (Group id : g.getInstances()) {
+					cs.sendMessage("  §7- " + id.getName());
+				}
+				cs.sendMessage("§aBerechtigungen:");
+				for (Permission p : g.getPermissions()) {
+					cs.sendMessage("  §7- " + p.getPermission() + "§7-§b" + p.getGroup());
+				}
+				cs.sendMessage("§aNegative Berechtigungen:");
+				for (Permission p : g.getNegativePerms()) {
+					cs.sendMessage("  §7- " + p.getPermission() + "§7-§b" + p.getGroup());
 				}
 				return;
-			}
-			else if(args[0].equalsIgnoreCase("groupReload")){
+			} else if (args[0].equalsIgnoreCase("groupReload")) {
 				Group g = PermissionManager.getManager().getGroup(args[1]);
-				if(g != null){
-					PermissionManager.getManager().getGroups().remove(g);
-					g = PermissionManager.getManager().addGroup(args[1]);
-					g.initPerms();
-					cs.sendMessage("§aGroup reloaded");
+				if (g == null) {
+					cs.sendMessage("§cGruppe nicht gefunden.");
+					return;
 				}
-				else
-				{
-					cs.sendMessage("§cGroup not found.");
-				}
+				PermissionManager.getManager().getGroups().remove(g);
+				g = PermissionManager.getManager().addGroup(args[1]);
+				g.initPerms();
+				cs.sendMessage("§aGruppe neugeladen.");
 				return;
-			}
-			else if(args[0].equalsIgnoreCase("addgroup")){
+			} else if (args[0].equalsIgnoreCase("addgroup")) {
 				Group g = PermissionManager.getManager().getGroup(args[1]);
-				if(g != null){
-					cs.sendMessage("§cGroup aredy exists!");
+				if (g != null) {
+					cs.sendMessage("§cDie Gruppe existiert bereits!");
 					return;
 				}
 				PermissionManager.getManager().addGroup(args[1]);
-				cs.sendMessage("§aGroup sucessfuly added!");
+				cs.sendMessage("§aGruppe erfolgreich hinzugefügt!");
 				return;
 			}
 		}
-		if(args.length >= 3){
-			if(args[0].equalsIgnoreCase("prefix")){
+		if (args.length >= 3) {
+			if (args[0].equalsIgnoreCase("prefix")) {
 				Group group = PermissionManager.getManager().getGroup(args[1]);
-				if(group == null){
-					cs.sendMessage("§cGroup not found!");
+				if (group == null) {
+					cs.sendMessage("§cGruppe nicht gefunden.");
 					return;
 				}
 				String prefix = "";
-				for(int i = 2;i<args.length;i++)
-					prefix+=" "+args[i];
-				if(prefix.startsWith(" '") && prefix.endsWith("'")){
-					group.setPrefix(prefix.replaceFirst(" '", "").substring(0, prefix.length()-3).replaceAll("&", "§"));
-					cs.sendMessage("§aPrefix gesetzt auf '"+group.getPrefix()+"'");
+				for (int i = 2; i < args.length; i++)
+					prefix += " " + args[i];
+				if (prefix.startsWith(" '") && prefix.endsWith("'")) {
+					group.setPrefix(ChatColor.translateAlternateColorCodes('&', prefix.replaceFirst(" '", "").substring(0, prefix.length() - 3)));
+					String prefixToDisplay = group.getPrefix();
+					String chatcolorDisplayed = "";
+					if (ChatColor.stripColor(prefixToDisplay).trim().isEmpty()) {
+						chatcolorDisplayed = " (ChatColor: " + prefixToDisplay.replace('§', '&') + " )";
+					}
+					cs.sendMessage("§aDas Prefix der Gruppe §6" + group.getName() + "§a wurde auf §6'§r" + prefixToDisplay + "§6'§a" + chatcolorDisplayed + " gesetzt.");
 					return;
-				}
-				else{
+				} else {
 					cs.sendMessage("§cBitte halte das Format ein!");
 					return;
 				}
-			}else if(args[0].equalsIgnoreCase("importance")){
+			} else if (args[0].equalsIgnoreCase("importance")) {
 				Group group = PermissionManager.getManager().getGroup(args[1]);
-				if(group == null){
-					cs.sendMessage("§cGroup not found!");
+				if (group == null) {
+					cs.sendMessage("§cGruppe nicht gefunden.");
 					return;
 				}
-				if(!StringUtils.isNumeric(args[2])){
-					cs.sendMessage("§cInvalid number!");
+				if (!StringUtils.isNumeric(args[2])) {
+					cs.sendMessage("§cBitte gebe eine gültige Zahl ein!");
 					return;
 				}
 				group.setImportance(Integer.parseInt(args[2]));
-				cs.sendMessage("§cYou set the importance to "+group.getImportance());
+				cs.sendMessage("§aDu hast die Importance der Gruppe §6" + group.getName() + "§a auf §6" + group.getImportance() + "§a gesetzt.");
 			}
 		}
-		if(args.length == 4){
-			if(args[0].equalsIgnoreCase("user")){
+		if (args.length == 4) {
+			if (args[0].equalsIgnoreCase("user")) {
 				UUID player = null;
-				if(args[2].matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")){
+				LoadedPlayer loadedPlayer = null;
+				if (args[2].matches("[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")) {
 					player = UUID.fromString(args[2]);
-				}
-				else if(args[2].length()<=16){
-					player = Main.getDatenServer().getClient().getPlayerAndLoad(args[2]).getUUID();
-				}
-				else{
-					cs.sendMessage("§cPlayer \""+args[2]+"\" not found");
+					loadedPlayer = Main.getDatenServer().getClient().getPlayerAndLoad(player);
+				} else if (args[2].length() <= 16) {
+					loadedPlayer = Main.getDatenServer().getClient().getPlayerAndLoad(args[2]);
+					player = loadedPlayer.getUUID();
+				} else {
+					cs.sendMessage("§cSpielername \"" + args[2] + "\" nicht valid.");
 					return;
 				}
 				PermissionPlayer pp = PermissionManager.getManager().getPlayer(player);
-
-				if(args[1].equalsIgnoreCase("addPerm")){
-					if(!pp.addPermission(args[3])){
-						cs.sendMessage("§cDie Permission konnte nicht hinzugefügt werden.");
+				
+				if (args[1].equalsIgnoreCase("addPerm")) {
+					if (!pp.addPermission(args[3])) {
+						cs.sendMessage("§cDie Berechtigung konnte dem Spieler §6" + loadedPlayer.getName() + "§c nicht hinzugefügt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Permission "+args[3]+" wurde hinzugefügt.");
+					cs.sendMessage("§aDie Berechtigung " + args[3] + " wurde dem Spieler §6" + loadedPlayer.getName() + "§a hinzugefügt.");
 					return;
-				}
-				else if(args[1].equalsIgnoreCase("removePerm")){
-					if(!pp.removePermission(args[3])){
-						cs.sendMessage("§cDie Permission konnte nicht entfernt werden.");
+				} else if (args[1].equalsIgnoreCase("removePerm")) {
+					if (!pp.removePermission(args[3])) {
+						cs.sendMessage("§cDie Berechtigung konnte dem Spieler §6" + loadedPlayer.getName() + "§c nicht entfernt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Permission "+args[3]+" wurde removed.");
+					cs.sendMessage("§aDie Berechtigung " + args[3] + " wurde dem Spieler §6" + loadedPlayer.getName() + "§a entfernt.");
 					return;
-				}
-				else if(args[1].equalsIgnoreCase("addGroup")){
-					if(!pp.addGroup(args[3])){
-						cs.sendMessage("§cDie gruppe "+args[3]+" konnte nicht hinzugefügt werden.");
+				} else if (args[1].equalsIgnoreCase("addGroup")) {
+					if (!pp.addGroup(args[3])) {
+						cs.sendMessage("§cDie Gruppe " + args[3] + " konnte dem Spieler §6" + loadedPlayer.getName() + "§c nicht hinzugefügt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Gruppe "+args[3]+" wurde hinzugefügt.");
+					cs.sendMessage("§aDie Gruppe " + args[3] + " wurde dem Spieler §6" + loadedPlayer.getName() + "§a hinzugefügt.");
 					return;
-				}
-				else if(args[1].equalsIgnoreCase("removeGroup")){
-					if(!pp.removeGroup(args[3])){
-						cs.sendMessage("§cDie Gruppe "+args[3]+" konnte nicht entfernt werden.");
+				} else if (args[1].equalsIgnoreCase("removeGroup")) {
+					if (!pp.removeGroup(args[3])) {
+						cs.sendMessage("§cDie Gruppe " + args[3] + " konnte dem Spieler §6" + loadedPlayer.getName() + "§c nicht entfernt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Gruppe "+args[3]+" wurde removed.");
+					cs.sendMessage("§aDie Gruppe " + args[3] + " wurde dem Spieler §6" + loadedPlayer.getName() + "§a entfernt.");
 					return;
 				}
-			}
-			else if(args[0].equalsIgnoreCase("group")){
+			} else if (args[0].equalsIgnoreCase("group")) {
 				Group g = PermissionManager.getManager().getGroup(args[2]);
-				if(g == null){
-					cs.sendMessage("§cThis group does not exist.");
+				if (g == null) {
+					cs.sendMessage("§cDiese Gruppe existiert nicht.");
 					return;
 				}
-				if(args[1].equalsIgnoreCase("addPerm")){
-					if(!g.addPermission(args[3])){
-						cs.sendMessage("§cDie Permission konnte nicht hinzugefügt werden.");
+				if (args[1].equalsIgnoreCase("addPerm")) {
+					if (!g.addPermission(args[3])) {
+						cs.sendMessage("§cDie Berechtigung konnte der Gruppe §6" + g.getName() + "§c nicht hinzugefügt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Permission "+args[3]+" wurde hinzugefügt.");
+					cs.sendMessage("§aDie Berechtigung §6" + args[3] + "§a wurde der Gruppe §6" + g.getName() + "§a hinzugefügt.");
 					return;
 				}
-				if(args[1].equalsIgnoreCase("removePerm")){
-					if(!g.removePermission(args[3])){
-						cs.sendMessage("§cDie Permission konnte nicht entfernt werden.");
+				if (args[1].equalsIgnoreCase("removePerm")) {
+					if (!g.removePermission(args[3])) {
+						cs.sendMessage("§cDie Berechtigung konnte der Gruppe §6" + g.getName() + "§c nicht entfernt werden.");
 						return;
 					}
-					cs.sendMessage("§aDie Permission "+args[3]+" wurde removed.");
+					cs.sendMessage("§aDie Berechtigung §6" + args[3] + "§a wurde der Gruppe §6" + g.getName() + "§a entfernt.");
 					return;
 				}
 			}
@@ -222,13 +216,13 @@ public class CommandPermission extends Command implements Listener {
 		cs.sendMessage("§7/perm groupInfo <Group>");
 		cs.sendMessage("§7/perm groupReload <Group>");
 		cs.sendMessage("§7/perm userInfo <Player/UUID>");
-
+		
 		cs.sendMessage("§7/perm group addPerm <group> <permission>");
 		cs.sendMessage("§7/perm group removePerm <group> <permission>");
-
+		
 		cs.sendMessage("§7/perm user addGroup <Player/UUID> <group>");
 		cs.sendMessage("§7/perm user removeGroup <Player/UUID> <group>");
-
+		
 		cs.sendMessage("§7/perm user addPerm <Player/UUID> <perm>");
 		cs.sendMessage("§7/perm user removePerm <Player/UUID> <perm>");
 	}
